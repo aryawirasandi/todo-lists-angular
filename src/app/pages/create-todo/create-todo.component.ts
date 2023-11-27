ce92f2d04e56786eaaed827a343762f17145f25c
aryawirasandi <arya.wirasandi@salt.co.id>
2023-11-27 15:15:00 +0800

    updat add component to template driven

--- src/app/pages/create-todo/create-todo.component.ts
+++ src/app/pages/create-todo/create-todo.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Todos } from '../../interfaces/todos';
 
 @Component({
   selector: 'app-create-todo',
   standalone: true,
   imports: [CommonModule, FormsModule],
+  styleUrl: './create-todo.component.scss',
   templateUrl: './create-todo.component.html',
 })
-export class CreateTodoComponent {}
+export class CreateTodoComponent {
+  todos: Todos = {
+    title: '',
+    description: '',
+    status: false,
+    createdAt: `${new Date()}`,
+  };
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+}
